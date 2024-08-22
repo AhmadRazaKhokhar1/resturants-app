6fbc1e84c5941c70e0e0379169b6238a07db1301
Ahmad Raza Khokhar <ahmadrazawebexpert@gmail.com>
2024-08-22 19:56:11 +0500

    update

---
+++ src/components/ResturantCards.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ResturantCards({resturantName, vicinity, user_ratings_total, rating, photoRefrence,icon, icon_background_color, open_now, business_status, geometry}) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
