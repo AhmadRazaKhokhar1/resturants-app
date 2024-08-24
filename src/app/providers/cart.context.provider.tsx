e4a95f87087c8d64866f8a2d3e2f855a99b81098
Ahmad Raza Khokhar <ahmadrazawebexpert@gmail.com>
2024-08-24 18:24:38 +0500

    update

---
+++ src/app/providers/cart.context.provider.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { ReactNode, useState } from "react";
+import { CartContext } from "../contexts/cart.context";
+import { SampleProductType } from "@/types/types";
+
+export default function CartProviderContext({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const [cart, setCart] = useState<SampleProductType[]>([]);
+
+  function addToCart(product: SampleProductType): void {
+    setCart((prev) => ([...prev, product]));
+  }
+
+  function removeFromCart(id: string): void {
+    const updatedCart = cart.filter(
+      (product: SampleProductType) => product.id != id
+    );
+    setCart(updatedCart);
+  }
+
+  const state = {
+    cart,
+    addToCart,
+    removeFromCart,
+  };
+
+  return (
+    <CartContext.Provider value={{ state }}>{children}</CartContext.Provider>
+  );
+}
