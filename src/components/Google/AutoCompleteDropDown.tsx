b5c935b1d8ff32adee8d8c6359a07be8581770a6
Ahmad Raza Khokhar <ahmadrazawebexpert@gmail.com>
2024-08-27 17:16:34 +0500

    mobile view update

--- src/components/Google/AutoCompleteDropDown.tsx
+++ src/components/Google/AutoCompleteDropDown.tsx
@@ -6,18 +6,18 @@ export default function AutoCompleteDropDown({
   getRestaurantsList,
 }: RestaurantCardCallBacksType) {
   return (
-    <div className="absolute flex lg:mt-[20%] lg:ml-[30%] md:mt-[40%] md:ml-[20%] sm:mt-[40%] sm:ml-[20%] ml-[8%] mt-[40%] outline outline-green-600 rounded-md shadow-2xl shadow-black items-center bg-white overflow-hidden p-0">
+    <div className="absolute flex w-80 sm:w-auto lg:mt-[20%] lg:ml-[30%] md:mt-[40%] md:ml-[20%] sm:mt-[40%] sm:ml-[20%] ml-[10%] mt-[40%] outline outline-green-600 rounded-md shadow-2xl shadow-black items-center bg-white overflow-hidden p-0">
       <input
         type="text"
         name="autocomplete"
         id="autocomplete"
         placeholder="Enter Destination"
-        className="p-0 sm:p-2 lg:p-3 xl:p-3 w-52 sm:w-72 lg:w-96 z-10 outline-none overflow-hidden"
+        className="p-0 sm:p-2 lg:p-3 xl:p-3 w-44 sm:w-72 lg:w-96 z-10 outline-none overflow-hidden"
       />
-      <button onClick={getCurrentUserLocation} className="p-1 bg-transparent" id="current-location">
+      <button onClick={getCurrentUserLocation} className="p-0 sm:p-1 bg-transparent" id="current-location">
         <TbCurrentLocation size={20} color="green" />
       </button>
-      <button className="bg-green-600 p-3" onClick={getRestaurantsList} id="get-restaurants">
+      <button className="bg-green-600 p-3 text-white" onClick={getRestaurantsList} id="get-restaurants">
         Get Restaurants
       </button>
     </div>
